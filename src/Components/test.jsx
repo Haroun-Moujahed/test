83a834ea0b879788c3615a7f2909178daefd11e2
Haroun-Moujahed <haroun.moujahed.tu@gmail.com>
2020-12-01 12:05:42 +0200

    first commit

---
+++ src/Components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function test() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h1></h1>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default test
